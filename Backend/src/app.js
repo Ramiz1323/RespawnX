28c1f06a108b5b19304e5f38488a2318ccd74d50
Sk Ramiz Raza <reenabibi5678@gmail.com>
2026-08-28 02:41:14 +0530

    feat: cors setup

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,12 +1,19 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import authRouter from "./routes/auth.routes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}))
 
 app.get("/", (req, res) => {
   res.status(200).json({
