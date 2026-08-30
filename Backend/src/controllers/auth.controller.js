afce2fc005302be9d7cb95e3dae935456d4ab48f
Sk Ramiz Raza <reenabibi5678@gmail.com>
2026-08-30 23:58:41 +0530

    feat(Backend): implement google oauth for register and login both conditions

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -3,51 +3,79 @@ import userModel from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
 async function sendTokenResponse(user, res, message) {
-    const token = jwt.sign({ _id: user._id }, config.JWT_SECRET, { expiresIn: "1d" })
+  const token = jwt.sign({ _id: user._id }, config.JWT_SECRET, {
+    expiresIn: "1d",
+  });
 
-    res.cookie("token", token);
+  res.cookie("token", token);
 
-    res.status(200).json({
-        message,
-        success: true,
-        user: {
-            id: user._id,
-            fullname: user.fullname,
-            email: user.email,
-            contact: user.contact,
-            role: user.role
-        }
-    })
+  res.status(200).json({
+    message,
+    success: true,
+    user: {
+      id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+      contact: user.contact,
+      role: user.role,
+    },
+  });
 }
 
 export const register = async (req, res) => {
-    const { email, contact, password, fullname, isSeller } = req.body;
+  const { email, contact, password, fullname, isSeller } = req.body;
 
-    try {
-        const existingUser = await userModel.findOne({
-            $or: [{ email }, { contact }]
-        })
-        if (existingUser) {
-            return res.status(400).json({ message: "User already exists" });
-        }
-
-        const user = await userModel.create({
-            email,
-            contact,
-            fullname,
-            password,
-            role: isSeller ? "seller" : "buyer"
-        })
-
-        await sendTokenResponse(user, res, "User registered successfully");
-
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Server error" });
+  try {
+    const existingUser = await userModel.findOne({
+      $or: [{ email }, { contact }],
+    });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
     }
-}
+
+    const user = await userModel.create({
+      email,
+      contact,
+      fullname,
+      password,
+      role: isSeller ? "seller" : "buyer",
+    });
+
+    await sendTokenResponse(user, res, "User registered successfully");
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
 
 export const googleCallback = async (req, res) => {
-    console.log(req.user);
-    res.redirect("http://localhost:5173/");
-}
+  const { id, displayName, emails, photos } = req.user;
+  // console.log(req.user);
+
+  const email = emails[0].value;
+  const profilePic = photos[0].value;
+
+  let user = await userModel.findOne({ email });
+
+  if (!user) {
+    user = await userModel.create({
+      email,
+      googleId: id,
+      fullname: displayName,
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    config.JWT_SECRET,
+    {
+      expiresIn: "7d",
+    },
+  );
+
+  res.cookie("token", token);
+
+  res.redirect("http://localhost:5173/");
+};
